1eb754c4e86df891b75a84a64e09a840ad82f133
nikthequik <quik.the.nick@gmail.com>
2017-02-20 14:54:46 -0700

    -Allow smooth scrolling to top

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 $(function() {
     console.log($(window).width());
 
+    /* Mobile Nav Collapse */
+
     $(window).scroll(function() {
         $(".collapse").collapse("hide");
     });
 
-    $(".nav-link").click(function() {
+    $(".nav-link a").click(function() {
         $(".collapse").collapse("hide");
     });
 
@@ -13,16 +15,26 @@ $(function() {
         $(".collapse").collapse("hide");
     });
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    /* Smooth Scrolling */
+
+    $('a[href*="#"]:not([href="#project-carousel"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
+            console.log(this.hash);
+            if (this.hash === "") {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 1000);
+                return false;
+            }
             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            
             if (target.length) {
                 $('html, body').animate({
-                scrollTop: target.offset().top
+                    scrollTop: target.offset().top
                 }, 1000);
                 return false;
             }
         }
     });
-});
+}); //END
